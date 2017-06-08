bc775c0a9d7a1e8dddeb4323481b8449f7ca62f5
Vaclav Rozsypalek <vaclav@giantswarm.io>
2017-06-08 15:46:36 +0200

    change variable names

--- pxemgr/config.go
+++ pxemgr/config.go
@@ -66,8 +66,8 @@ type Network struct {
 	Router       string
 	DNS          []string
 	PXE          bool
-	Subnet       string `yaml:"subnet_len"`
-	Vlan         string `yaml:"vlan_id"`
+	SubnetSize   string `yaml:"subnet_size"`
+	VlanId       string `yaml:"vlan_id"`
 	NetworkModel string `yaml:"network_model"`
 
 	IgnoredHosts []string
