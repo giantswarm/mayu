0e66ff21b9d26fa250f95d09d8505818d86b5dab
Vaclav Rozsypalek <calvin_24@seznam.cz>
2017-07-28 10:19:06 +0200

    Specify bond type (#108)
    * specify-bond-type
    
    * update net templates
    
    * yaml def

--- pxemgr/config.go
+++ pxemgr/config.go
@@ -71,6 +71,7 @@ type Network struct {
 	SubnetSize   string `yaml:"subnet_size"`
 	VlanId       string `yaml:"vlan_id"`
 	NetworkModel string `yaml:"network_model"`
+	BondMode     string `yaml:"bond_mode"`
 
 	IgnoredHosts []string
 	StaticHosts  []hostmgr.IPMac
