4d15e24b2a16340441c67b855d14233363987bc2
Federico Gimenez <fgimenez@canonical.com>
2017-08-22 09:14:38 +0200

    address review comments (thanks Tim)

--- pxemgr/pxemanager_test.go
+++ pxemgr/pxemanager_test.go
@@ -33,51 +33,50 @@ after_key: ok
 `
 )
 
-var (
+type helper struct {
 	dir      string
 	fakeEtcd *httptest.Server
 	pxeCfg   PXEManagerConfiguration
 	req      *http.Request
 	w        *httptest.ResponseRecorder
 	cluster  *hostmgr.Cluster
-)
+}
 
-func setUp(t *testing.T) {
+func setUp(t *testing.T) *helper {
+	h := &helper{}
+
 	var err error
-	dir, err = ioutil.TempDir("", "pxmgr_cloudconfig_")
+	h.dir, err = ioutil.TempDir("", "pxmgr_cloudconfig_")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err := ioutil.WriteFile(filepath.Join(dir, "config_ok.yaml"), []byte(configOK), 0644); err != nil {
+	if err := ioutil.WriteFile(filepath.Join(h.dir, "config_ok.yaml"), []byte(configOK), 0644); err != nil {
 		t.Fatal(err)
 	}
-	if err := ioutil.WriteFile(filepath.Join(dir, "config_err.yaml"), []byte(configErr), 0644); err != nil {
+	if err := ioutil.WriteFile(filepath.Join(h.dir, "config_err.yaml"), []byte(configErr), 0644); err != nil {
 		t.Fatal(err)
 	}
-	if err := ioutil.WriteFile(filepath.Join(dir, "last_stage_cloudconfig.yaml"), []byte(lastStageCloudconfig), 0644); err != nil {
+	if err := ioutil.WriteFile(filepath.Join(h.dir, "last_stage_cloudconfig.yaml"), []byte(lastStageCloudconfig), 0644); err != nil {
 		t.Fatal(err)
 	}
 
-	cluster, err = hostmgr.NewCluster(dir, true)
+	h.cluster, err = hostmgr.NewCluster(h.dir, true)
 	if err != nil {
 		t.Fatalf("creating cluster: %s", err)
 	}
 
-	fakeEtcd = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	h.fakeEtcd = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "{}")
 	}))
 
-	pxeCfg = PXEManagerConfiguration{
+	h.pxeCfg = PXEManagerConfiguration{
 		UseInternalEtcdDiscovery: true,
 		NoTLS:                true,
 		APIPort:              4080,
 		PXEPort:              4081,
-		BindAddress:          "0.0.0.0",
-		UseIgnition:          false,
-		LastStageCloudconfig: filepath.Join(dir, "last_stage_cloudconfig.yaml"),
-		Version:              "1.0.0",
-		EtcdEndpoint:         fakeEtcd.URL,
+		LastStageCloudconfig: filepath.Join(h.dir, "last_stage_cloudconfig.yaml"),
+		EtcdEndpoint:         h.fakeEtcd.URL,
 	}
 
 	hostData := machinedata.HostData{
@@ -85,32 +84,34 @@ func setUp(t *testing.T) {
 	}
 	b := new(bytes.Buffer)
 	json.NewEncoder(b).Encode(hostData)
-	req = httptest.NewRequest("POST", "http://127.0.0.1:4080/final-cloud-config.yaml", b)
-	w = httptest.NewRecorder()
+	h.req = httptest.NewRequest("POST", "http://127.0.0.1:4080/final-cloud-config.yaml", b)
+	h.w = httptest.NewRecorder()
+
+	return h
 }
 
-func tearDown() {
-	os.RemoveAll(dir)
-	fakeEtcd.Close()
+func tearDown(h *helper) {
+	os.RemoveAll(h.dir)
+	h.fakeEtcd.Close()
 }
 
 func TestFinalCloudConfigChecksErrorOk(t *testing.T) {
-	setUp(t)
-	defer tearDown()
+	h := setUp(t)
+	defer tearDown(h)
 
-	pxeCfg.ConfigFile = filepath.Join(dir, "config_ok.yaml")
-	mgr, err := PXEManager(pxeCfg, cluster)
+	h.pxeCfg.ConfigFile = filepath.Join(h.dir, "config_ok.yaml")
+	mgr, err := PXEManager(h.pxeCfg, h.cluster)
 	if err != nil {
 		t.Fatalf("unable to create a pxe manager: %s\n", err)
 	}
-	mgr.configGenerator(w, req)
+	mgr.configGenerator(h.w, h.req)
 
-	if status := w.Code; status != http.StatusOK {
+	if status := h.w.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
 
-	actual := w.Body.String()
+	actual := h.w.Body.String()
 	expected := `before_key: ok
 storage: mystorage
 after_key: ok
@@ -127,24 +128,24 @@ after_key: ok
 }
 
 func TestFinalCloudConfigChecksErrorFail(t *testing.T) {
-	setUp(t)
-	defer tearDown()
+	h := setUp(t)
+	defer tearDown(h)
 
-	pxeCfg.ConfigFile = filepath.Join(dir, "config_err.yaml")
-	mgr, err := PXEManager(pxeCfg, cluster)
+	h.pxeCfg.ConfigFile = filepath.Join(h.dir, "config_err.yaml")
+	mgr, err := PXEManager(h.pxeCfg, h.cluster)
 	if err != nil {
 		t.Fatalf("unable to create a pxe manager: %s\n", err)
 	}
-	mgr.configGenerator(w, req)
+	mgr.configGenerator(h.w, h.req)
 
-	if status := w.Code; status != http.StatusInternalServerError {
+	if status := h.w.Code; status != http.StatusInternalServerError {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusInternalServerError)
 	}
 
-	actual := w.Body.String()
-	expected := `generating final stage cloudConfig failed: template: last_stage_cloudconfig.yaml:2:5: executing "last_stage_cloudconfig.yaml" at <eq .TemplatesEnv.sto...>: error calling eq: invalid type for comparison`
-	if actual != expected {
+	actual := h.w.Body.String()
+	expected := `generating final stage cloudConfig failed: template: last_stage_cloudconfig.yaml`
+	if !strings.Contains(actual, expected) {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			actual, expected)
 	}
