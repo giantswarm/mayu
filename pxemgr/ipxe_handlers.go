a39dab566a395346e35e5a86b09566ac2aa7e743
Vaclav Rozsypalek <vaclav@giantswarm.io>
2017-12-04 17:24:45 +0100

    debug build

--- pxemgr/ipxe_handlers.go
+++ pxemgr/ipxe_handlers.go
@@ -28,9 +28,9 @@ func (mgr *pxeManagerT) ipxeBootScript(w http.ResponseWriter, r *http.Request) {
 	buffer := bytes.NewBufferString("")
 
 	// for ignition we use only 1phase installation without mayu-infopusher
-	kernel := fmt.Sprintf("kernel %s/images/vmlinuz coreos.first_boot=1 initrd=initrd.cpio.gz console=ttyS0,115200n8 coreos.config.url=%s?uuid=${uuid}&serial=${serial} systemd.journald.max_level_console=debug debug ignore_loglevel log_buf_len=10M print_fatal_signals=1 LOGLEVEL=8 earlyprintk=vga,keep sched_debug initcall_debug udev.log_priority=8 \n", mgr.pxeURL(), mgr.ignitionURL())
+	kernel := fmt.Sprintf("kernel %s/images/vmlinuz coreos.first_boot=1 initrd=initrd.cpio.gz coreos.config.url=%s?uuid=${uuid}&serial=${serial} systemd.journald.max_level_console=debug debug ignore_loglevel log_buf_len=10M coreos.autologin=tty0\n", mgr.pxeURL(), mgr.ignitionURL())
 	initrd := fmt.Sprintf("initrd %s/images/initrd.cpio.gz\n", mgr.pxeURL())
-
+// console=ttyS0,115200n8
 	buffer.WriteString("#!ipxe\n")
 	buffer.WriteString("dhcp\n")
 	buffer.WriteString("echo " + kernel)
