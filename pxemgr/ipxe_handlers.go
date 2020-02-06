3c82a2290112b715bbe7fb9ca611a9413f9bfe22
Simon Weald <glitchcrab-github@simonweald.com>
2020-02-06 13:26:40 +0000

    update boot args for flatcar

--- pxemgr/ipxe_handlers.go
+++ pxemgr/ipxe_handlers.go
@@ -36,12 +36,12 @@ func (mgr *pxeManagerT) ipxeBootScript(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if mgr.coreosAutologin {
-		extraFlags += " coreos.autologin"
-		mgr.logger.Log("level", "info", "message", "adding coreos.autologin to kernel args")
+		extraFlags += " flatcar.autologin"
+		mgr.logger.Log("level", "info", "message", "adding flatcar.autologin to kernel args")
 	}
 
 	// for ignition we use only 1phase installation without mayu-infopusher
-	kernel := fmt.Sprintf("kernel %s/images/vmlinuz coreos.first_boot=1 initrd=initrd.cpio.gz coreos.config.url=%s?uuid=${uuid}&serial=${serial} systemd.journald.max_level_console=debug verbose log_buf_len=10M "+extraFlags+"\n", mgr.pxeURL(), mgr.ignitionURL())
+	kernel := fmt.Sprintf("kernel %s/images/vmlinuz flatcar.first_boot=1 initrd=initrd.cpio.gz ignition.config.url=%s?uuid=${uuid}&serial=${serial} systemd.journald.max_level_console=debug verbose log_buf_len=10M "+extraFlags+"\n", mgr.pxeURL(), mgr.ignitionURL())
 	initrd := fmt.Sprintf("initrd %s/images/initrd.cpio.gz\n", mgr.pxeURL())
 	// console=ttyS0,115200n8
 	buffer.WriteString("#!ipxe\n")
