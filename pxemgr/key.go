6bd8ba55dbb43d6a25b1a28eb32bbc784b3ab2c2
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-06-02 16:11:31 +0300

    Update images keys

--- pxemgr/key.go
+++ pxemgr/key.go
@@ -6,14 +6,14 @@ import (
 )
 
 const (
-	vmlinuzFile = "coreos_production_pxe.vmlinuz"
-	initrdFile  = "coreos_production_pxe_image.cpio.gz"
+	vmlinuzFile = "flatcar_production_pxe.vmlinuz"
+	initrdFile  = "flatcar_production_pxe_image.cpio.gz"
 )
 
-func (mgr *pxeManagerT) pxeKernelImage(coreOSversion string) (*os.File, error) {
-	return os.Open(path.Join(mgr.imagesCacheDir+"/"+coreOSversion, vmlinuzFile))
+func (mgr *pxeManagerT) pxeKernelImage(flatcarVersion string) (*os.File, error) {
+	return os.Open(path.Join(mgr.imagesCacheDir+"/"+flatcarVersion, vmlinuzFile))
 }
 
-func (mgr *pxeManagerT) pxeInitRD(coreOSversion string) (*os.File, error) {
-	return os.Open(path.Join(mgr.imagesCacheDir+"/"+coreOSversion, initrdFile))
+func (mgr *pxeManagerT) pxeInitRD(flatcarVersion string) (*os.File, error) {
+	return os.Open(path.Join(mgr.imagesCacheDir+"/"+flatcarVersion, initrdFile))
 }
